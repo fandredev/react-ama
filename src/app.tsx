3adba896f12825f65945c77469d78b07f896b74e
felipe <profissionalf.andre@gmail.com>
2024-08-08 11:19:48 -0300

    feat: add client from react query to use and consume requests

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,9 +19,9 @@ const router = createBrowserRouter([
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />;
       <Toaster invert richColors />
-    </>
+    </QueryClientProvider>
   );
 }
