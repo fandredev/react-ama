253539807f7705cab37bb7548ee319ec6e17d024
felipe <profissionalf.andre@gmail.com>
2024-08-08 11:26:40 -0300

    feat: add form create message and integration with backend and react query

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 
 import amaLogo from "../assets/ama-logo.svg";
-import { ArrowRight, Share2 } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 import { Messages } from "../components/messages";
 import { Suspense } from "react";
+import { CreateMessageForm } from "../components/create-message-form";
 
 export function Room() {
   const { roomId } = useParams();
@@ -42,22 +43,7 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-orange focus-within:ring-1">
-        <input
-          type="text"
-          name="theme"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm mx-2 bg-transparent outline-none placeholder:text-zinc-500 text-zinc-100"
-        />
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center transition-colors rounded-lg font-medium text-sm hover:bg-orange-500"
-        >
-          Criar pergunta
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
+      <CreateMessageForm />
 
       <Suspense fallback={<p>Loading...</p>}>
         <Messages />
