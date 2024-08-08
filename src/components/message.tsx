0e10b79d30635f89f57d353c7c7dd458285303d1
felipe <profissionalf.andre@gmail.com>
2024-08-08 11:37:27 -0300

    feat: implements like and deslike from messages

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,23 +1,53 @@
 import { ArrowUp } from "lucide-react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import { createMessageReaction } from "../http/create-message-reaction";
+import { removeMessageReaction } from "../http/remove-message-reaction";
 
 interface MessageProps {
+  id: string;
   text: string;
   amountOfReactions: number;
   answered?: boolean;
 }
 
 export function Message({
+  id: messageId,
   text,
   amountOfReactions,
   answered = false,
 }: MessageProps) {
   const [hasReacted, setHasReacted] = useState(false);
+  const { roomId } = useParams();
 
-  function handleReactToMessage() {
-    setHasReacted((prev) => !prev);
+  if (!roomId) {
+    throw new Error("Messages components must be used within a room pages");
   }
 
+  async function createMessageReactionAction() {
+    if (!roomId) return;
+
+    try {
+      await createMessageReaction({ roomId, messageId });
+    } catch {
+      toast.error("An error occurred while reacting to the message.");
+    }
+
+    setHasReacted(true);
+  }
+
+  async function removeMessageReactionAction() {
+    if (!roomId) return;
+
+    try {
+      await removeMessageReaction({ roomId, messageId });
+    } catch {
+      toast.error("An error occurred while unreacting to the message.");
+    }
+
+    setHasReacted(false);
+  }
   return (
     <li
       data-answered={answered}
@@ -26,6 +56,7 @@ export function Message({
       {text}
       {hasReacted ? (
         <button
+          onClick={removeMessageReactionAction}
           type="button"
           className="flex items-center mt-3 gap-2 text-orange-400 text-small font-medium hover:text-orange-500"
         >
@@ -34,7 +65,7 @@ export function Message({
         </button>
       ) : (
         <button
-          onClick={handleReactToMessage}
+          onClick={createMessageReactionAction}
           type="button"
           className="flex items-center mt-3 gap-2 text-zinc-400 text-small font-medium hover:text-zinc-300"
         >
